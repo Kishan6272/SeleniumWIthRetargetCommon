5ddf2fa9ff66285f331c9f2161948e36f9cc2926
Kishan6272 <kishanpandey6272@gmail.com>
2025-08-17 20:43:58 +0530

    loisojo

--- src/main/java/AAAAAAA11111111/learningGit.java
+++ src/main/java/AAAAAAA11111111/learningGit.java
@@ -14,6 +14,8 @@ public class learningGit {
         System.out.print(arr[i]+" ");
         System.out.print(arr[1]+" ");
         System.out.print(arr[2]+" ");System.out.print(arr[3]+" ");
+        System.out.print(arr[1]+" ");
+        System.out.print(arr[2]+" ");System.out.print(arr[3]+" ");
 
 
     }
