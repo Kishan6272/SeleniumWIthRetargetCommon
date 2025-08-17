163c5290f4389a6cf9225e5fe600faac68e84aea
Kishan6272 <kishanpandey6272@gmail.com>
2025-08-17 20:39:43 +0530

    new print statement

--- src/main/java/AAAAAAA11111111/learningGit.java
+++ src/main/java/AAAAAAA11111111/learningGit.java
@@ -12,5 +12,9 @@ public class learningGit {
 
         for (int i=0;i<arr.length;i++)
         System.out.print(arr[i]+" ");
+        System.out.print(arr[1]+" ");
+        System.out.print(arr[2]+" ");System.out.print(arr[3]+" ");
+
+
     }
 }
