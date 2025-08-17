1bfe65a37efa534d587a42acf953b5b2a98fba1d
Kishan6272 <kishanpandey6272@gmail.com>
2025-08-17 20:30:25 +0530

    array addition

--- src/main/java/AAAAAAA11111111/learningGit.java
+++ src/main/java/AAAAAAA11111111/learningGit.java
@@ -1,7 +1,16 @@
 package AAAAAAA11111111;
 
+import java.lang.reflect.Array;
+
+import static java.lang.System.in;
+
 public class learningGit {
     public static void main(String[] args) {
+
         System.out.println("kishan ");
+        int []arr={1,2,3,4,5};
+
+        for (int i=0;i<arr.length;i++)
+        System.out.print(arr[i]+" ");
     }
 }
